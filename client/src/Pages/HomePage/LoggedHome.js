d860254ce555e294687471e077398debd97d20fd
MashodRana <mashod0rana@gmail.com>
2022-12-25 23:35:10 +0600

    desgin updated.

--- client/src/Pages/HomePage/LoggedHome.js
+++ client/src/Pages/HomePage/LoggedHome.js
@@ -8,7 +8,7 @@ const LoggedHome = () => {
                     <div class="flex items-center space-x-4 p-2 mb-5">
                         <img class="h-12 rounded-full" src="http://www.gravatar.com/avatar/2acfb745ecf9d4dccb3364752d17f65f?s=260&d=mp" alt="James Bhatta"/>
                             <div>
-                                <h4 class="font-semibold text-lg text-gray-700 capitalize font-poppins tracking-wide">James Bhatta</h4>
+                                <h4 class="font-semibold text-lg text-gray-700 capitalize font-poppins tracking-wide">Mashod Rana</h4>
                                 <span class="text-sm tracking-wide flex items-center space-x-1">
                                     <svg class="h-4 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
@@ -34,7 +34,7 @@ const LoggedHome = () => {
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"></path>
                                     </svg>
                                 </span>
-                                <span>Notifications</span>
+                                <span>Sale Products</span>
                             </a>
                         </li>
                         <li>
@@ -44,7 +44,7 @@ const LoggedHome = () => {
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path>
                                     </svg>
                                 </span>
-                                <span>Personal messages</span>
+                                <span>Products</span>
                             </a>
                         </li>
                         <li>
@@ -54,7 +54,7 @@ const LoggedHome = () => {
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                                     </svg>
                                 </span>
-                                <span>My profile</span>
+                                <span>Sales</span>
                             </a>
                         </li>
                         <li>
@@ -64,7 +64,7 @@ const LoggedHome = () => {
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
                                     </svg>
                                 </span>
-                                <span>My orders</span>
+                                <span>Purchase</span>
                             </a>
                         </li>
                         <li>
