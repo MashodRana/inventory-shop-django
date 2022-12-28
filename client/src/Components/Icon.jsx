e098a673c47d79aa35484adc8a16a0415a4dfa3a
MashodRana <mashod0rana@gmail.com>
2022-12-28 22:36:30 +0600

    menu item added to submenu

--- client/src/Components/Icon.jsx
+++ client/src/Components/Icon.jsx
@@ -82,7 +82,7 @@ export const ProductsIcon = () => {
     )
 }
 
-export const Supplier = () => {
+export const SupplierIcon = () => {
     return (
         <>
             <svg
--- client/src/Components/SideMenu.jsx
+++ client/src/Components/SideMenu.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChartPie, faFileInvoice, faJarWheat } from '@fortawesome/free-solid-svg-icons'
-import { BillingIcon, DashboardIcon, PurchaseIcon, SalesIcon } from "./Icon";
+import { AccountBookIcon, BillingIcon, DashboardIcon, PaymentsIcon, ProductsIcon, PurchaseIcon, SalesIcon, SupplierIcon } from "./Icon";
 
 const SideMenu = () => {
   return (
@@ -79,7 +79,7 @@ const SideMenu = () => {
                 aria-controls="dropdown-example"
                 data-collapse-toggle="dropdown-example"
               >
-                <SalesIcon/>
+                <SalesIcon />
                 <span
                   class="flex-1 ml-3 text-left whitespace-nowrap"
                   sidebar-toggle-item
@@ -128,7 +128,7 @@ const SideMenu = () => {
                 aria-controls="dropdown-example2"
                 data-collapse-toggle="dropdown-example2"
               >
-                <PurchaseIcon/>
+                <PurchaseIcon />
                 <span
                   class="flex-1 ml-3 text-left whitespace-nowrap"
                   sidebar-toggle-item
@@ -176,9 +176,202 @@ const SideMenu = () => {
                 </li>
               </ul>
             </li>
-            
-            
-            
+
+            {/* Products */}
+            <li>
+              <button
+                type="button"
+                class="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                aria-controls="dropdown-example3"
+                data-collapse-toggle="dropdown-example3"
+              >
+                <ProductsIcon />
+                <span
+                  class="flex-1 ml-3 text-left whitespace-nowrap"
+                  sidebar-toggle-item
+                >
+                  Products
+                </span>
+                <svg
+                  sidebar-toggle-item
+                  class="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+              <ul id="dropdown-example3" class="hidden py-2 space-y-2">
+                <li>
+                  <Link
+                    to="view-products"
+                    class="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                  >
+                    View Products
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="add-proudcts"
+                    class="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                  >
+                    Add Products
+                  </Link>
+                </li>
+              </ul>
+            </li>
+
+            {/* Suppliers */}
+            <li>
+              <button
+                type="button"
+                class="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                aria-controls="dropdown-example4"
+                data-collapse-toggle="dropdown-example4"
+              >
+                <SupplierIcon />
+                <span
+                  class="flex-1 ml-3 text-left whitespace-nowrap"
+                  sidebar-toggle-item
+                >
+                  Suppliers
+                </span>
+                <svg
+                  sidebar-toggle-item
+                  class="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+              <ul id="dropdown-example4" class="hidden py-2 space-y-2">
+                <li>
+                  <Link
+                    to="view-supplier"
+                    class="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                  >
+                    View Suppliers
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="add-proudcts"
+                    class="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                  >
+                    Add Suppliers
+                  </Link>
+                </li>
+              </ul>
+            </li>
+
+            {/* Payments */}
+            <li>
+              <button
+                type="button"
+                class="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                aria-controls="dropdown-example5"
+                data-collapse-toggle="dropdown-example5"
+              >
+                <PaymentsIcon />
+                <span
+                  class="flex-1 ml-3 text-left whitespace-nowrap"
+                  sidebar-toggle-item
+                >
+                  Payments
+                </span>
+                <svg
+                  sidebar-toggle-item
+                  class="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+              <ul id="dropdown-example5" class="hidden py-2 space-y-2">
+                <li>
+                  <Link
+                    to="received-payments"
+                    class="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                  >
+                    Received Payments
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="payoff-payments"
+                    class="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                  >
+                    Payoff Payments
+                  </Link>
+                </li>
+              </ul>
+            </li>
+
+            {/* Account Book */}
+            <li>
+              <button
+                type="button"
+                class="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                aria-controls="dropdown-example6"
+                data-collapse-toggle="dropdown-example6"
+              >
+                <AccountBookIcon />
+                <span
+                  class="flex-1 ml-3 text-left whitespace-nowrap"
+                  sidebar-toggle-item
+                >
+                  Account Book
+                </span>
+                <svg
+                  sidebar-toggle-item
+                  class="w-6 h-6"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </button>
+              <ul id="dropdown-example6" class="hidden py-2 space-y-2">
+                <li>
+                  <Link
+                    to="cutomer-acount-book"
+                    class="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                  >
+                    Customer
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="supplier-account-book"
+                    class="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                  >
+                    Supplier
+                  </Link>
+                </li>
+              </ul>
+            </li>
           </ul>
         </div>
       </aside>
