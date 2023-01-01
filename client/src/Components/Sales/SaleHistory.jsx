7525f693371d9c2db23f2d6b5c4a73f0e4ddb87f
MashodRana <mashod0rana@gmail.com>
2023-01-01 20:08:26 +0600

    sales history component skeleton

---
+++ client/src/Components/Sales/SaleHistory.jsx
@@ -0,0 +1,9 @@
+const SaleHistory = ()=>{
+    return (
+        <>
+        <h1 className="text-2xl">This is sales history.</h1>
+        </>
+    );
+};
+
+export default SaleHistory;
--- client/src/Components/SideMenu.jsx
+++ client/src/Components/SideMenu.jsx
@@ -103,7 +103,7 @@ const SideMenu = () => {
               <ul id="dropdown-example" class="hidden py-2 space-y-2">
                 <li>
                   <Link
-                    to="sale-histoty"
+                    to="sale-history"
                     class="flex items-center p-2 pl-11 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                   >
                     Sale History
