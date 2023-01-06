0da45821b1f6741cc58b51fea86fa6887965fbb6
MashodRana <mashod0rana@gmail.com>
2023-01-06 22:31:45 +0600

    barcode and product search input field design done

--- client/src/Components/Bill/Bill.jsx
+++ client/src/Components/Bill/Bill.jsx
@@ -1,4 +1,4 @@
-import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
+import { faBarcode, faSearch, faUserPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
 
@@ -35,6 +35,35 @@ const Bill = () => {
                         </div>
                     </div>
 
+                    {/* barcode */}
+                    <div class="md:flex md:items-center md:justify-start mb-6">
+                        <div class="flex md:w-3/6">
+                            <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                                <FontAwesomeIcon icon={faBarcode} />
+                            </span>
+                            <input 
+                            type="text" 
+                            id="website-admin" 
+                            class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            placeholder="Barcode" 
+                            />
+                        </div>
+                    </div>
+
+                    {/* Search product */}
+                    <div class="md:flex md:items-center md:justify-start mb-6">
+                        <div class="flex md:w-3/6">
+                            <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+                                <FontAwesomeIcon icon={faSearch} />
+                            </span>
+                            <input 
+                            type="text" 
+                            id="website-admin" 
+                            class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            placeholder="Search product by Name" 
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
