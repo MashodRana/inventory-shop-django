92743f42807b33d7c8fe042e239404c01903d7f4
MashodRana <mashod0rana@gmail.com>
2023-01-11 23:09:53 +0600

    purchase model implented

--- server/purchases/models.py
+++ server/purchases/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from common.models import TimeStamp
+from suppliers.models import Supplier
+
+
 # Create your models here.
+class Purchase(TimeStamp):
+    supplier = models.ForeignKey(to=Supplier, on_delete=models.SET_NULL, null=True)
+    detail = models.TextField()
+    total_price = models.FloatField()
+
+    def __str__(self):
+        return f"order-{self.pk}"
