1e398138e0e5129bc66536fb738ccb3b6728eb13
MashodRana <mashod0rana@gmail.com>
2023-01-11 10:21:50 +0600

    supplier model and refrencing done

--- server/products/models.py
+++ server/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from suppliers.models import Supplier
 
 
 class Unit(models.Model):
@@ -14,7 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=63)
     brand = models.CharField(max_length=50)
     unit = models.ForeignKey(to=Unit, on_delete=models.SET_NULL, null=True)
-    supplier = models.CharField(max_length=50)
+    supplier = models.ForeignKey(to=Supplier, on_delete=models.SET_NULL, null=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- server/suppliers/models.py
+++ server/suppliers/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Supplier(models.Model):
+    supplier_code = models.CharField(max_length=63)
+    name = models.CharField(max_length=31)
+    designation = models.CharField(max_length=15)
+    primary_phone = models.CharField(max_length=15)
+    secondary_phone = models.CharField(max_length=15, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    company = models.CharField(max_length=31)
+    address = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def  __str__(self) -> str:
+        return self.name    
