3c029a4653b5553595acc81f337f68619ac3ecbe
MashodRana <mashod0rana@gmail.com>
2023-01-11 23:43:26 +0600

    urls file added to common and product app

--- server/inventory_of_shop/urls.py
+++ server/inventory_of_shop/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('common.urls')),
-    path('', include('product')),
+    path('', include('product.urls')),
 ]
