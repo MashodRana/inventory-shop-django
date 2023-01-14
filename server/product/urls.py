b9241ed5963f7a817e6d5134aa17456b9a3a9b7f
MashodRana <mashod0rana@gmail.com>
2023-01-14 22:17:12 +0600

    product details view and url

--- server/product/urls.py
+++ server/product/urls.py
@@ -4,4 +4,6 @@ from product import views
 
 urlpatterns = [
     path('products/', views.ProductListView.as_view(), name='products'),
+    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
+
 ]
