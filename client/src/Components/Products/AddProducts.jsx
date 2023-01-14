317cb61e4f7685b35e5ed24575f681c5f8e71272
MashodRana <mashod0rana@gmail.com>
2023-01-15 00:02:48 +0600

    product adding done

--- client/src/Components/Products/AddProducts.jsx
+++ client/src/Components/Products/AddProducts.jsx
@@ -1,90 +1,144 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState, useEffect } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const AddProducts = () => {
+  const [productInfo, setProductInfo] = useState({});
+  const [units, setUnits] = useState([]);
+  const unitUrl = `http://127.0.0.1:8000/units/`;
+  const productsUrl = `http://127.0.0.1:8000/products/`
+
+  const handleOnChange = (evnt) => {
+    const field = evnt.target.name;
+    const value = evnt.target.value;
+    let newData = { ...productInfo };
+    newData[field] = value;
+    setProductInfo(newData);
+  }
+
+  const handleAddProduct = async () => {
+    const response = await fetch(productsUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(productInfo)
+    })
+
+    if (response.status === 201) {
+      setProductInfo({ 'name': "", 'brand': '', "unit": 1, 'detail': '' })
+      toast.success('Product adding successfull!', {
+        position: toast.POSITION.TOP_RIGHT
+      });
+    }
+    else {
+      toast.error('Prodcut adding failed!', {
+        position: toast.POSITION.TOP_RIGHT
+      });
+      setProductInfo(productInfo)
+    }
+  }
+
+  useEffect(() => {
+    // Load units from the server
+    fetch(unitUrl)
+      .then(res => res.json())
+      .then(data => setUnits(data))
+  }, [unitUrl])
+
   return (
     <>
-      <h1 className="text-start sm:text-3xl text-xl font-medium title-font text-gray-900">
-        New Product
-      </h1>
+      {console.log(productInfo)}
+      <h1 className="text-start sm:text-3xl text-xl font-medium title-font text-gray-900"> New Product</h1>
       <p className="text-md text-start">
         Add new products with details information.
       </p>
-      <div class="container px-5 py-6 mx-auto">
+      <div className="container px-5 py-6 mx-auto">
         {/* product selection */}
         <div className="grid grid-cols-3 gap-4 py-4">
           <div className="flex">
-            <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+            <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
               Name
             </span>
             <input
               type="text"
               id="website-admin"
-              class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              name='name'
+              value={productInfo.name}
+              onChange={handleOnChange}
+              className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Product name"
             />
           </div>
         </div>
-        <div class="grid grid-cols-3 gap-4 py-4">
-          <div class="flex">
-            <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+        <div className="grid grid-cols-3 gap-4 py-4">
+          <div className="flex">
+            <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
               Brand
             </span>
-            <select
-              id="underline_select"
-              class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            >
-              <option selected>Choose brand</option>
-              <option value="kg">Uniliver</option>
-              <option value="l">RFL</option>
-              <option value="p">Akiz</option>
-            </select>
+            <input
+              type="text"
+              id="website-admin"
+              name='brand'
+              value={productInfo.brand}
+              onChange={handleOnChange}
+              className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Product brand"
+            />
           </div>
 
-          <div class="flex">
-            <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+          <div className="flex">
+            <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
               Unit
             </span>
             <select
               id="underline_select"
-              class="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              name='unit'
+              defaultValue={productInfo.unit}
+              onChange={handleOnChange}
+              className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             >
-              <option selected>Choose brand</option>
-              <option value="kg">KG</option>
+              {units.length ? units.map((unit, idx) => (<option key={idx} value={unit.id}>{unit.title}</option>)) : ""}
+              {/* <option value="kg">KG</option>
               <option value="l">Litter</option>
-              <option value="p">Pices</option>
+              <option value="p">Pices</option> */}
             </select>
           </div>
-          
+
         </div>
         <div >
-            <label
-              for="message"
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-start"
-            >
-              Product Details
-            </label>
-            <textarea
-              id="message"
-              rows="4"
-              class="block p-2.5 md:w-1/2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Write your thoughts here..."
-            ></textarea>
-          </div>
+          <label
+            htmlFor="message"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white text-start"
+          >
+            Product Details
+          </label>
+          <textarea
+            id="message"
+            name="detail"
+            value={productInfo.detail}
+            onChange={handleOnChange}
+            rows="4"
+            className="block p-2.5 md:w-1/2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Write your thoughts here..."
+          ></textarea>
+        </div>
       </div>
 
       <div className="py-4 flex justify-center items-center">
         <button
+          onClick={handleAddProduct}
           type="button"
-          class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-amber-200 hover:bg-amber-200 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+          className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-amber-200 hover:bg-amber-200 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
         >
-          <FontAwesomeIcon
-            className="text-xl text-gray-500 hover:cursor-pointer"
-            icon={faPlus}
-          />{" "}
+          <FontAwesomeIcon className="text-xl text-gray-500 hover:cursor-pointer" icon={faPlus} />{" "}
           Add Product
         </button>
       </div>
+
+      {/* Toaster to show confirmation message */}
+      <ToastContainer />
     </>
   );
 };
