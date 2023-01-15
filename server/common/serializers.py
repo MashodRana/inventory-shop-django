51bc1a2e27966bd73c47b3108f30d99a978f222f
MashodRana <mashod0rana@gmail.com>
2023-01-15 12:25:14 +0600

    serializer fiexed

--- server/common/serializers.py
+++ server/common/serializers.py
@@ -6,4 +6,4 @@ from common.models import Unit
 class UnitSerializer(serializers.ModelSerializer):
     class Meta:
         model = Unit
-        fields = ['short_form', 'title', 'detail']
+        fields = "__all__"
