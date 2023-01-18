62208a61de8430e4bdf311c2074c1b0ddd261a4b
MashodRana <mashod0rana@gmail.com>
2023-01-18 11:27:52 +0600

    purchased products

--- server/purchase/models.py
+++ server/purchase/models.py
@@ -7,8 +7,13 @@ from supplier.models import Supplier
 # Create your models here.
 class Purchase(TimeStamp):
     supplier = models.ForeignKey(to=Supplier, on_delete=models.SET_NULL, null=True)
+<<<<<<< Updated upstream
     detail = models.TextField()
     total_price = models.FloatField()
+=======
+    note = models.TextField()
+    total = models.FloatField()
+>>>>>>> Stashed changes
 
     def __str__(self):
         return f"order-{self.pk}"
--- server/purchase/views.py
+++ server/purchase/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+<<<<<<< Updated upstream
+=======
+class PurchaseView(APIView):
+    def get(self, request, format=None):
+        purchases = Purchase.objects.all()
+        serializer = PurchaseSerializer(purchases, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = PurchaseSerializer(data=request.data)
+        if serializer.is_valid():
+            purchase = serializer.save()
+            for i, product in enumerate(request.data['products']):
+                request.data['products'][i]['bill_no'] = purchase.id
+
+            serializer = PurchasedProductSerializer(data=request.data['products'], many=True)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=HTTP_201_CREATED)
+            else:
+                purchase.delete()
+        print(serializer.errors)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+
+
+class PurchasedProductsView(APIView):
+    def get(self, request, format=None):
+        purchased_products = PurchasedProduct.objects.all()
+        serializer = PurchasedProductSerializer(purchased_products, many=True)
+        return Response(serializer.data)
+>>>>>>> Stashed changes
