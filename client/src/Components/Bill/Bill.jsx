88ff6b56b8df1019980e7a3790825fabe5d2f9eb
MashodRana <mashod0rana@gmail.com>
2023-01-31 23:37:35 +0600

    user dropdown added

--- client/src/Components/Bill/Bill.jsx
+++ client/src/Components/Bill/Bill.jsx
@@ -35,11 +35,14 @@ const Bill = () => {
                             </label>
                         </div>
                         <div class="md:w-2/6">
-                            <select class="inline appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                                <option>New Mexico</option>
-                                <option>Missouri</option>
-                                <option>Texas</option>
-                            </select>
+                            <Select
+                                onChange={handleProudctSelection}
+                                className="inline appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                options={products.map((product) => ({
+                                    value: product.id,
+                                    label: product.name,
+                                }))}
+                            />
                             <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /></svg>
                             </div>
